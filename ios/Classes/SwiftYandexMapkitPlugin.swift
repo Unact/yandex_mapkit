886d3c41f243b61eb6ad77618ac449b3201c66fd
Dzhafar <dsishokov@gmail.com>
2019-05-27 16:55:57 +0300

    refactor code

--- ios/Classes/SwiftYandexMapkitPlugin.swift
+++ ios/Classes/SwiftYandexMapkitPlugin.swift
@@ -1,17 +1,10 @@
 import CoreLocation
 import Flutter
 import UIKit
-import YandexMapKit
 
 public class SwiftYandexMapkitPlugin: NSObject, FlutterPlugin {
-  static var channel: FlutterMethodChannel!
-  static var controller: YandexMapController!
-  static var pluginRegistrar: FlutterPluginRegistrar!
 
   public static func register(with registrar: FlutterPluginRegistrar) {
-    channel = FlutterMethodChannel(name: "yandex_mapkit", binaryMessenger: registrar.messenger())
-
-    registrar.addMethodCallDelegate(SwiftYandexMapkitPlugin(), channel: channel)
     registrar.register(
       YandexMapFactory(registrar: registrar),
       withId: "yandex_mapkit/yandex_map"
