76fad7c17fd3ce88d85640c50b6db3ce828c3547
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-07-30 10:53:49 +0300

    Remove unused code

--- lib/yandex_mapkit.dart
+++ lib/yandex_mapkit.dart
@@ -1,18 +1,10 @@
 library yandex_map;
 
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 export 'src/map_animation.dart';
 export 'src/placemark.dart';
 export 'src/point.dart';
 export 'src/yandex_map.dart';
 export 'src/yandex_map_controller.dart';
 
-/// Singleton Class for accessing Yandex MapView
-/// To initialize use `setup`
-/// To communicate with MapView use `map` property
 class YandexMapkit {
 }
