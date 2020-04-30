90c7dda875722550cccb7d503f81bbbb837eea89
goolpe <gooolpe@gmail.com>
2020-04-30 17:24:55 +0300

    added equatable

--- lib/src/point.dart
+++ lib/src/point.dart
@@ -1,8 +1,21 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class Point {
-  const Point({@required this.latitude, @required this.longitude});
+class Point extends Equatable{
+  const Point({
+    @required this.latitude, 
+    @required this.longitude
+  });
 
   final double latitude;
   final double longitude;
+
+  @override
+  List<Object> get props => <Object>[
+    latitude,
+    longitude
+  ];
+
+  @override
+  bool get stringify => true;
 }
