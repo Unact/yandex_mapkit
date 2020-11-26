a39513cedecf34ff75160c96b99a4d7ef1635e52
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2020-11-26 17:51:13 +0300

    Update Android initialization to match flutters recommended structure

--- README.md
+++ README.md
@@ -108,12 +108,13 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.yandex.mapkit.MapKitFactory;
 
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     MapKitFactory.setApiKey("YOUR_API_KEY");
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    super.configureFlutterEngine(flutterEngine);
   }
 }
 ```
--- example/android/app/src/main/java/com/unact/yandexmapkitexample/MainActivity.java
+++ example/android/app/src/main/java/com/unact/yandexmapkitexample/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 import com.yandex.mapkit.MapKitFactory;
 
@@ -12,6 +11,6 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     MapKitFactory.setApiKey("YOUR_API_KEY");
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    super.configureFlutterEngine(flutterEngine);
   }
 }
