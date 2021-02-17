9a76348b2376b8c387a19e36504112eb1e0367a4
Tatiana Belova <t.belousova@mediasoft.team>
2021-02-17 12:37:11 +0400

    Change output to print

--- example/lib/examples/map_controls_page.dart
+++ example/lib/examples/map_controls_page.dart
@@ -160,23 +160,18 @@ class _MapControlsExampleState extends State<_MapControlsExample> {
                   )
                 ]),
                 TableRow(
-                    children: <Widget>[
-                      ControlButton(
-                        onPressed: () async {
-                          final Map<String, Point> region =
-                              await controller.getVisibleRegion();
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                  'TopLeft: ${region['topLeftPoint']},\nBottomRight: ${region['bottomRightPoint']}'),
-                            ),
-                          );
-                        },
-                        title: 'Visible map region'),
-                      const TextButton(onPressed: null, child: Text(''))
-                    ],
-                  ),
-                ],
+                  children: <Widget>[
+                    ControlButton(
+                      onPressed: () async {
+                        final Map<String, Point> region =
+                            await controller.getVisibleRegion();
+                        print('TopLeft: ${region['topLeftPoint']}, BottomRight: ${region['bottomRightPoint']}');
+                      },
+                      title: 'Visible map region'),
+                    const TextButton(onPressed: null, child: Text(''))
+                  ],
+                ),
+              ],
             ),
           ),
         )
