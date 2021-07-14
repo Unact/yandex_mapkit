8da8490a953b3745414325850e313a46c0d3a5ea
Roman Solovev <s.romeo.a@gmail.com>
2021-07-14 15:02:58 +0300

    Tilt gestures switching

--- android/src/main/java/com/unact/yandexmapkit/YandexMapController.java
+++ android/src/main/java/com/unact/yandexmapkit/YandexMapController.java
@@ -574,6 +574,19 @@ public class YandexMapController implements PlatformView, MethodChannel.MethodCa
     );
   }
 
+  private boolean isTiltGesturesEnabled() {
+    return mapView.getMap().isTiltGesturesEnabled();
+  }
+
+  public void setTiltGesturesEnabled(MethodCall call) {
+
+    Map<String, Object> params = ((Map<String, Object>) call.arguments);
+
+    boolean enabled = (Boolean) params.get("enabled");
+
+    mapView.getMap().setTiltGesturesEnabled(enabled);
+  }
+
   @Override
   public void onMethodCall(MethodCall call, MethodChannel.Result result) {
     switch (call.method) {
@@ -677,6 +690,12 @@ public class YandexMapController implements PlatformView, MethodChannel.MethodCa
         Map<String, Object> userTargetPoint = getUserTargetPoint();
         result.success(userTargetPoint);
         break;
+      case "isTiltGesturesEnabled":
+        boolean isTiltGesturesEnabled = isTiltGesturesEnabled();
+        result.success(isTiltGesturesEnabled);
+      case "setTiltGesturesEnabled":
+        setTiltGesturesEnabled(call);
+        result.success(null);
       default:
         result.notImplemented();
         break;
