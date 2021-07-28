8376e0aeff4f1a7037999d3acd82927186300086
il_agent <rvverh@mail.ru>
2021-07-28 22:18:09 +0300

    return registrar

--- android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
+++ android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
@@ -7,6 +7,7 @@ import com.yandex.mapkit.MapKitFactory;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class YandexMapkitPlugin implements FlutterPlugin {
   private static final String VIEW_TYPE = "yandex_mapkit/yandex_map";
@@ -14,6 +15,22 @@ public class YandexMapkitPlugin implements FlutterPlugin {
 
   private MethodChannel methodChannel;
   private YandexSearchHandlerImpl handler;
+
+  public static void registerWith(Registrar registrar) {
+
+    if (registrar.activity() == null) {
+      // When a background flutter view tries to register the plugin, the registrar has no activity.
+      // We stop the registration process as this plugin is foreground only.
+      return;
+    }
+
+    MapKitFactory.initialize(registrar.activity().getApplicationContext());
+    MapKitFactory.getInstance().onStart();
+
+    registrar.platformViewRegistry().registerViewFactory(VIEW_TYPE, new YandexMapFactory(registrar.messenger()));
+
+    new YandexMapkitPlugin().setupYandexSearchChannel(registrar.messenger(), registrar.context());
+  }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
