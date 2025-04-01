123f8eb814eabaaa5d3457e2c7f19736cc039647
khus <107538974+khusvr@users.noreply.github.com>
2025-04-01 12:33:40 +0500

    Update YandexMapController.swift isM1Simulator

--- ios/Classes/lite/YandexMapController.swift
+++ ios/Classes/lite/YandexMapController.swift
@@ -233,7 +233,11 @@ public class YandexMapController:
   }
 
   private static func isM1Simulator() -> Bool {
-    return (TARGET_IPHONE_SIMULATOR & TARGET_CPU_ARM64) != 0
+      #if arch(arm64) && os(iOS) && targetEnvironment(simulator)
+      return true
+      #else
+      return false
+      #endif
   }
 
   private func hasLocationPermission() -> Bool {
